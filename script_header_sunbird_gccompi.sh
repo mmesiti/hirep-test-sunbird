93b57cd8ad18b0d4f4126827a9544a7476bee744
Michele Mesiti <michele.mesiti@gmail.com>
2021-01-20 11:42:43 +0000

    Even more time

--- script_header_sunbird_gccompi.sh
+++ script_header_sunbird_gccompi.sh
@@ -3,7 +3,7 @@
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=1
 #SBATCH -A scw1019
-#SBATCH -t 120 
+#SBATCH -t 180 
 #SBATCH -J HRTSSED_I # HiRep Test Suite
 #SBATCH -o outSED_I.txt
 #SBATCH -e errSED_I.txt
