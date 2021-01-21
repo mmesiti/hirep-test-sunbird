385e421a0379614c854e8cd25903714b03ffcdee
Michele Mesiti <michele.mesiti@gmail.com>
2021-01-21 14:51:11 +0000

    env variables should be picked up.

--- run_tests_old.sh
+++ run_tests_old.sh
@@ -5,17 +5,37 @@
 
 ../Make/Utils/write_mkflags.pl -f ../Make/MkFlags $@ || exit 1
 
+if [ -z "${CC}" ]
+then
+    CCMAKE=""
+    echo "CC not set"
+else
+    CCMAKE="CC=${CC}"
+    echo "CC=${CC}"
+fi
+
+if [ -z "${MPI_CC}" ]
+then
+    MPICCMAKE=""
+    echo "MPICC not set"
+else
+    MPICCMAKE="MPICC=${MPI_CC}"
+    echo "MPICC=${MPI_CC}"
+fi
+
+
+
 if [[ " $@ " =~ ' --gauge[[:space:]]SPN ' ]]
 then
     echo Run SpN regression tests...
-    make -C SPNtoSUNRegression runtests
+    make $CCMAKE $MPICCMAKE -C SPNtoSUNRegression runtests
 fi
 
 echo Cleaning...
 ( cd .. && make cleanall )
 
 echo Building...
-make -j1 tests
+make $CCMAKE $MPICCMAKE -j1 tests
 
 echo Run Tests...
-make runalltests
+make $CCMAKE $MPICCMAKE runalltests
