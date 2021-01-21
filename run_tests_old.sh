11bcdf54a293d86662afac524b211e642d3b84d6
Michele Mesiti <michele.mesiti@gmail.com>
2021-01-21 15:22:09 +0000

    Removed MPICCMAKE variable

--- run_tests_old.sh
+++ run_tests_old.sh
@@ -14,28 +14,17 @@ else
     echo "CC=${CC}"
 fi
 
-if [ -z "${MPI_CC}" ]
-then
-    MPICCMAKE=""
-    echo "MPICC not set"
-else
-    MPICCMAKE="MPICC=${MPI_CC}"
-    echo "MPICC=${MPI_CC}"
-fi
-
-
-
 if [[ " $@ " =~ ' --gauge[[:space:]]SPN ' ]]
 then
     echo Run SpN regression tests...
-    make $CCMAKE $MPICCMAKE -C SPNtoSUNRegression runtests
+    make $CCMAKE -C SPNtoSUNRegression runtests
 fi
 
 echo Cleaning...
 ( cd .. && make cleanall )
 
 echo Building...
-make $CCMAKE $MPICCMAKE -j1 tests
+make $CCMAKE -j1 tests
 
 echo Run Tests...
-make $CCMAKE $MPICCMAKE runalltests
+make $CCMAKE runalltests
